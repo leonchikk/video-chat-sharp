83214821cfd428d0acf5d34ef08f68cfcd9a8491
Illia Suzyma <Illia@festagency.com>
2022-02-23 19:02:49 +0300

    Added Opus.NET library for codec

---
+++ Multimedia.Video.Desktop/VideoDeviceManager.cs
@@ -0,0 +1,56 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VideoChat.Core.Models;
+using VideoChat.Core.Multimedia;
+
+namespace Multimedia.Video.Desktop
+{
+    public class VideoDeviceManager : IVideoDeviceManager
+    {
+        private IEnumerable<VideoDeviceInfo> _captureDevices;
+        public IEnumerable<VideoDeviceInfo> AvailableDevices => _captureDevices;
+
+
+        private IVideoDevice _videoDevice;
+        public IVideoDevice CurrentDevice => _videoDevice;
+
+
+        public VideoDeviceManager()
+        {
+            _captureDevices = (
+                GetDevices()
+                    .Select(x => new VideoDeviceInfo(x.Name, x.MonikerString))
+             ).ToList();
+
+            Setup();
+        }
+
+        public void SwitchTo(VideoDeviceInfo device)
+        {
+            if (device == null)
+                throw new ArgumentNullException(nameof(device));
+
+            _videoDevice = new VideoDevice(_captureDevices.First());
+        }
+
+        private void Setup()
+        {
+            if (!_captureDevices.Any())
+                return;
+
+            SwitchTo(_captureDevices.First());
+        }
+
+        private IEnumerable<FilterInfo> GetDevices()
+        {
+            var devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            for (int i = 0; i < devices.Count; i++)
+            {
+                yield return devices[i];
+            }
+        }
+    }
+}
---
+++ VideoChat.Core/Multimedia/IVideoDeviceManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using VideoChat.Core.Models;
+
+namespace VideoChat.Core.Multimedia
+{
+    public interface IVideoDeviceManager
+    {
+        IEnumerable<VideoDeviceInfo> AvailableDevices { get; }
+        IVideoDevice CurrentDevice { get; }
+        void SwitchTo(VideoDeviceInfo device);
+    }
+}
