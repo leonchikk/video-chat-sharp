21145e4fc254b0523a98ae04658f91bf921bd3ba
Illia Suzyma <Illia@festagency.com>
2022-02-23 23:48:14 +0300

    Minor changes

--- OpusWrapper/OpusDecoder.cs
+++ OpusWrapper/OpusDecoder.cs
@@ -41,7 +41,7 @@ namespace FragLabs.Audio.Codecs
             _decoder = decoder;
             OutputSamplingRate = outputSamplingRate;
             OutputChannels = outputChannels;
-            MaxDataBytes = 8000;
+            MaxDataBytes = 6000;
         }
 
         /// <summary>
